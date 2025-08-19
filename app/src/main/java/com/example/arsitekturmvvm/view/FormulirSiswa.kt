d7a5843c427621786bf899f03c54f4c992bbf5f3
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-19 15:22:53 +0700

    edit var alamat dan menambah var gender, listdata

--- app/src/main/java/com/example/arsitekturmvvm/view/FormulirSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/FormulirSiswa.kt
@@ -19,9 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,7 +42,9 @@ fun FormulirSiswa(
 ){
     //edit 2: tambahkan 4 variable
     var txtNama by rememberSaveable { mutableStateOf("") }
-    var textAlamat by remember { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    var listData: MutableList<String> = mutableListOf(txtNama,txtGender,txtAlamat)
 
     Scaffold(modifier = Modifier,
         { TopAppBar(
