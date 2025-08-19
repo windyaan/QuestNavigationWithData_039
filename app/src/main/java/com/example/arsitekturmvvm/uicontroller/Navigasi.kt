dff7642da2a7573616701d8393429c093021fdd4
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-19 16:13:45 +0700

    tambah parameter, variable konteks, dan edit FormulirSiswa

--- app/src/main/java/com/example/arsitekturmvvm/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/arsitekturmvvm/uicontroller/Navigasi.kt
@@ -3,12 +3,15 @@ package com.example.arsitekturmvvm.uicontroller
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.arsitekturmvvm.model.DataJK.JenisK
 import com.example.arsitekturmvvm.view.FormulirSiswa
 import com.example.arsitekturmvvm.view.TampilSiswa
 import com.example.arsitekturmvvm.viewmodel.SiswaViewModel
@@ -25,15 +28,20 @@ fun SiswaApp(
     navController: NavHostController = rememberNavController()
 ){
     Scaffold { isiRuang->
+        //edit 2: tambahkan variable uiState
+        val uiState = viewModel.statusUI.collectAsState()
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulir.name,
 
             modifier = Modifier.padding(isiRuang)){
             composable(route = Navigasi.Formulir.name){
-                FormIsian (
-                    //pilihanJK = JenisK.map{ id -> konteks.resources.getString(id) },
-                    OnSubmitBtnClick = {
+                //edit 3: tambahkan var konteks
+                val konteks = LocalContext.current
+                FormulirSiswa (
+                    //edit 4: parameter pilihanJK dan onSubmitButtonClicked
+                    pilihanJK = JenisK.map{ id -> konteks.resources.getString(id) },
+                    onSubmitButtonClicked = {
                         navController.navigate(Navigasi.Detail.name)
                     }
                 )
