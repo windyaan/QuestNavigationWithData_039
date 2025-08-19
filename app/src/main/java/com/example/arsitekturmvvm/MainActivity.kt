e8e7788a678e5c49a430b63c469599a4518c3d83
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-19 18:50:23 +0700

    main program

--- app/src/main/java/com/example/arsitekturmvvm/MainActivity.kt
+++ app/src/main/java/com/example/arsitekturmvvm/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.arsitekturmvvm.ui.theme.ArsitekturMVVMTheme
 import com.example.arsitekturmvvm.uicontroller.SiswaApp
-import com.example.arsitekturmvvm.viewmodel.SiswaViewModel
 
 //class MainActivity : ComponentActivity() {
 //    override fun onCreate(savedInstanceState: Bundle?) {
