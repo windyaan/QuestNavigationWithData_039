7fa1437e8b23acb0444d64db5a6b223594b00bdc
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-19 18:38:49 +0700

    gradle dan libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.arsitekturmvvm"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.arsitekturmvvm"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
