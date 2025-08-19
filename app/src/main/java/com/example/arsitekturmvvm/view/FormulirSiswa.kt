7ddac135067623a41334e54a72515f87d4eebe6e
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-19 15:10:22 +0700

    Formulir

--- app/src/main/java/com/example/arsitekturmvvm/view/FormulirSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/FormulirSiswa.kt
@@ -1,2 +1,94 @@
 package com.example.arsitekturmvvm.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.DividerDefaults.Thickness
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.arsitekturmvvm.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+    Scaffold(modifier = Modifier,
+        { TopAppBar(
+            title = { Text(stringResource(id = R.string.home), color = Color.White)},
+            colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
+        )}
+    ){
+            isiRuang ->
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = { Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
+            HorizontalDivider(modifier = Modifier
+                .padding(top = 20.dp)
+                .width(250.dp), thickness = Thickness, color = Color.Red)
+            Row {
+                jenisK.forEach {
+                        item ->
+                    Row(verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
+            HorizontalDivider(modifier = Modifier
+                .padding(top = 20.dp)
+                .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(250.dp),
+                label = { Text(text = "Alamat")},
+                onValueChange = {},
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(1f)
+                    .padding(all = 25.dp),
+                onClick = OnSubmitBtnClick
+            ) {
+                Text(text = stringResource(id = R.string.submit))
+            }
+        }
+    }
+}
